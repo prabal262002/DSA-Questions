3d4f9715c151a8d089763b4308cfafdd42d06a3d
prabal262002 <86048405+prabal262002@users.noreply.github.com>
2023-08-02 14:14:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (70.64%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,21 +14,17 @@
  * }
  */
 class Solution {
-    int maxlevel = 0;
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list  = new ArrayList<>();
-        right(root,1,list);
+        right(root,0,list);
         return list ;
     }
 
     void right(TreeNode root,int level,List<Integer> list){
-        if(root==null){
-            return ;
-        }
-        if(maxlevel<level){
-            list.add(root.val);
-            maxlevel=level;
-        }
+        if(root==null) return;
+
+        if(level== list.size())   list.add(root.val);
+
         right(root.right,level+1,list);
         right(root.left,level+1,list);
         
